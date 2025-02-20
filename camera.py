7af97daa491a3073683c4f7852c171d57f2a4ac9
Michele <mikyvita2004@gmail.com>
2025-02-20 12:15:44 +0100

    Fixing bugs

--- camera.py
+++ camera.py
@@ -6,5 +6,5 @@ class Cam:
     def __init__(self):
         self.cam = Camera()
         print("Camera caricata!")
-        self.cam.start.preview()
+        self.cam.start_preview()
         sleep(5)
